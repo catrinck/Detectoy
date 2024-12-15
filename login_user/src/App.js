7b2d4d92d059fcc579ee5c3422de49ea13ae3a43
aglisonnn <aglisonmn@gmail.com>
2024-12-15 17:28:03 -0400

    login front

--- login_user/src/App.js
+++ login_user/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="login-form-wrap">
+          <form className="login-form"> 
+            <input type = "email" name = "email" placeholder="Email" required/>
+            <input type = "password" name = "password" placeholder="Senha" required/>
+            <button type = "submit" className="btn-login">Login</button>
+            <button type = "submit" className="btn-cadastro">Não tem login? Cadastre-se</button>
+          </form>
+        </div>
       </header>
     </div>
   );
