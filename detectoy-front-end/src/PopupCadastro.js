dc82114deb36a5295bcc9ed35088773f58e76d71
Oziel Bezerra <obdl.snf23@uea.edu.br>
2025-03-25 19:01:14 -0400

    Popup de confirmação

--- detectoy-front-end/src/PopupCadastro.js
+++ detectoy-front-end/src/PopupCadastro.js
@@ -1,4 +1,3 @@
-// PopupCadastro.js
 import React, { useState } from 'react';
 import axios from "axios";
 import './home-page/TelaInicial.css';
@@ -7,6 +6,8 @@ function PopupCadastro({ onClose }) {
 
     const [administradorChecked, setAdministradorChecked] = useState(false);
     const [supervisorChecked, setSupervisorChecked] = useState(false);
+    const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const [usuario, setUsuario] = useState({
         cpf: "",
@@ -16,7 +17,6 @@ function PopupCadastro({ onClose }) {
         log: false,
         cameras: false,
         relatorios: false,
-
     });
 
     const handleInputChange = (e) => {
@@ -25,6 +25,7 @@ function PopupCadastro({ onClose }) {
             ...prevState,
             [name]: value,
         }));
+        setErrorMessage("");
     };
 
     const handleAdministradorChange = () => {
@@ -52,70 +53,86 @@ function PopupCadastro({ onClose }) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://127.0.0.1:8000/api/funcionarios/", usuario);
-            alert(response.status + " - Usuário criado: " + usuario.nome);
+            await axios.post("http://127.0.0.1:8000/api/funcionarios/", usuario);
+            setShowSuccessPopup(true);  // Mostra o pop-up de sucesso
         } catch (error) {
-            alert("Erro ao criar o usuário: " + error.message);
+            setErrorMessage("CPF ou email inválidos!");
         }
     };
 
     return (
         <div className="popup-overlay">
-            <div className="popup-content">
-                <h2>Adicionar usuário</h2>
-                <form>
-                    <input
-                        type="text"
-                        placeholder="Nome"
-                        name="nome"
-                        value={usuario.nome}
-                        onChange={handleInputChange}
-                        className="cadastro-input"
-                    />
-                    <input
-                        type="text"
-                        placeholder="CPF"
-                        name="cpf"
-                        value={usuario.cpf}
-                        onChange={handleInputChange}
-                        className="cadastro-input"
-                    />
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        value={usuario.email}
-                        onChange={handleInputChange}
-                        className="cadastro-input"
-                    />
-                    <input
-                        type="password"
-                        placeholder="Senha"
-                        name="senha"
-                        value={usuario.senha}
-                        onChange={handleInputChange}
-                        className="cadastro-input"
-                    />
-                    <div className="checkboxPermissions">
+            {/* Só mostra o formulário se o pop-up de sucesso NÃO estiver visível */}
+            {!showSuccessPopup && (
+                <div className="popup-content">
+                    <h2>Adicionar usuário</h2>
+                    <form>
                         <input
-                            type="checkbox"
-                            id="administrador"
-                            checked={administradorChecked}
-                            onChange={handleAdministradorChange}
+                            type="text"
+                            placeholder="Nome"
+                            name="nome"
+                            value={usuario.nome}
+                            onChange={handleInputChange}
+                            className="cadastro-input"
                         />
-                        <label htmlFor="administrador">Administrador</label>
                         <input
-                            type="checkbox"
-                            id="supervisor" checked={supervisorChecked}
-                            onChange={handleSupervisorChange} 
+                            type="text"
+                            placeholder="CPF"
+                            name="cpf"
+                            value={usuario.cpf}
+                            onChange={handleInputChange}
+                            className="cadastro-input"
                         />
-                            
-                        <label htmlFor="supervisor">Supervisor</label>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            value={usuario.email}
+                            onChange={handleInputChange}
+                            className="cadastro-input"
+                        />
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            name="senha"
+                            value={usuario.senha}
+                            onChange={handleInputChange}
+                            className="cadastro-input"
+                        />
+                        <div className="checkboxPermissions">
+                            <input
+                                type="checkbox"
+                                id="administrador"
+                                checked={administradorChecked}
+                                onChange={handleAdministradorChange}
+                            />
+                            <label htmlFor="administrador">Administrador</label>
+                            <input
+                                type="checkbox"
+                                id="supervisor" 
+                                checked={supervisorChecked}
+                                onChange={handleSupervisorChange} 
+                            />
+                            <label htmlFor="supervisor">Supervisor</label>
+                        </div>
+                        {errorMessage && <p className="error-message">{errorMessage}</p>}
+                        <button type="submit" onClick={handleSubmit} className="btnCadastroUser">Cadastrar</button>
+                    </form>
+                    <button className="close-button" onClick={onClose}>Fechar</button>
+                </div>
+            )}
+
+            {/* Pop-up de sucesso aparece e esconde o outro */}
+            {showSuccessPopup && (
+                <div className="success-popup">
+                    <div className="success-content">
+                        <h3>Usuário Cadastrado!</h3>
+                        <button onClick={() => { setShowSuccessPopup(false); onClose(); }}>
+                            Voltar à Usuários
+                        </button>
                     </div>
-                    <button type="submit" onClick={handleSubmit} className="btnCadastroUser">Cadastrar</button>
-                </form>
-                <button className="close-button" onClick={onClose}>Fechar</button>
-            </div>
+                </div>
+            )}
         </div>
     );
 }
